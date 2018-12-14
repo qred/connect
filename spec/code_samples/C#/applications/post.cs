e0f238456ae1c7684f477b85a26f14e93610220c
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2018-12-14 14:39:04 +0100

    Documentation and code example

--- spec/code_samples/C#/applications/post.cs
+++ spec/code_samples/C#/applications/post.cs
@@ -1,12 +1,32 @@
-API.v1.Echo echo = new API.v1.Echo();
-echo.message = "Hello World!");
-EchoResponse response = echo.post();
-if (response.statusCode == HttpStatusCode.Created)
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace AwesomeBusinessApp
 {
-  // Success
+  public class CreatedLoanApplication
+  {
+    public string Id { get; set; }
+    public string Url { get; set; }
+  }
+
+  public class ConnectToQred
+  {
+    HttpClient client = new HttpClient(); // note: You might not want to do this but inject client through dependency injection
+    public async Task<CreatedLoanApplication> Connect()
+    {
+      var payload = new {
+          Amount=5000,
+          Organization=new {TaxID="SE2344473928"},
+          Applicant=new{Email="joe.smith@a-company.com", Telephone="408-867-5309"}
+      };
+      var result = await client.PostAsync("https://api.qred.com/loans/v1/applications", new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json"));
+      result.EnsureSuccessStatusCode();
+
+      return JsonConvert.DeserializeObject<CreatedLoanApplication>(await result.Content.ReadAsStringAsync());
+    } 
+  }
 }
-else
-{
-  // Something wrong -- check response for errors
-  Console.WriteLine(response.getRawResponse());
-}
+
+
