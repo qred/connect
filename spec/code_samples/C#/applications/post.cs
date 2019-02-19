60be63e1e80794767c4229db265d426645cb060b
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-02-19 16:32:27 +0100

    Cleanup code example

--- spec/code_samples/C#/applications/post.cs
+++ spec/code_samples/C#/applications/post.cs
@@ -9,6 +9,7 @@ add Identity Model package:
 
 ``` 
 dotnet add package IdentityModel 
+dotnet add package Qred.Connect
 ```
 
 You should end up with something like:
@@ -22,6 +23,7 @@ You should end up with something like:
   </PropertyGroup>
   <ItemGroup>
     <PackageReference Include="IdentityModel" Version="3.10.4" />
+    <PackageReference Include="Qred.Connect" Version="0.2.0" />
   </ItemGroup>
 
 </Project>
@@ -36,137 +38,10 @@ using System.Threading.Tasks;
 using IdentityModel.Client;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Qred.Connect;
 
 namespace AwesomeBusinessApp
 {
-  public class TypeNameSerializationBinder : ISerializationBinder
-  {
-    public TypeNameSerializationBinder()
-    {
-      var typeNames = new Dictionary<Type, string>
-            {
-                { typeof(ApplicationRequest),"simple" },
-                { typeof(ApplicationApplicant),"simple" },
-                { typeof(ApplicationOrganization),"simple" },
-            };
-      foreach (var typeName in typeNames)
-      {
-        Map(typeName.Key, typeName.Value);
-      }
-    }
-
-    readonly Dictionary<Type, string> typeToName = new Dictionary<Type, string>();
-
-    public void Map(Type type, string name) => this.typeToName.Add(type, name);
-
-    public void BindToName(Type serializedType, out string assemblyName, out string typeName)
-    {
-      var name = typeToName.GetValueOrDefault(serializedType, null);
-      if (name != null)
-      {
-        assemblyName = null;
-        typeName = name;
-      }
-      else
-      {
-        assemblyName = null;
-        typeName = null;
-      }
-    }
-
-    public Type BindToType(string assemblyName, string typeName) => null;
-  }
-
-  public class ApplicationApplicant
-  {
-    public string Email { get; set; }
-
-    public string Phone { get; set; }
-
-    public string NationalIdentificationNumber { get; set; }
-
-    public string GivenName { get; set; }
-
-    public string AdditionalName { get; set; }
-
-    public string FamilyName { get; set; }
-  }
-
-  public class ApplicationOrganization
-  {
-    public string NationalOrganizationNumber { get; set; }
-
-    public string Email { get; set; }
-
-    public string Phone { get; set; }
-
-    public string Url { get; set; }
-  }
-
-  public class ApplicationRequest
-  {
-    public decimal? Amount { get; set; }
-
-    public string PromoCode { get; set; }
-
-    public decimal? Term { get; set; }
-
-    public string PurposeOfLoan { get; set; }
-
-    public ApplicationOrganization Organization { get; set; }
-
-    public ApplicationApplicant Applicant { get; set; }
-
-    public List<UploadBase64File> Files { get; set; }
-
-    /// <summary>
-    /// Get the JSON string presentation of the application request
-    /// </summary>
-    /// <returns>JSON string presentation of the application request</returns>
-    public string ToJson()
-    {
-      return JsonConvert.SerializeObject(this, Formatting.Indented, new JsonSerializerSettings
-      {
-        SerializationBinder = new TypeNameSerializationBinder(),
-        TypeNameHandling = TypeNameHandling.Objects,
-        ContractResolver = new DefaultContractResolver
-        {
-          NamingStrategy = new CamelCaseNamingStrategy
-          {
-            OverrideSpecifiedNames = false
-          }
-        }
-      });
-    }
-  }
-
-  public class UploadBase64File
-  {
-    public List<string> EncodingFormat { get; set; }
-
-    public string Filename { get; set; }
-
-    public byte[] Base64Content { get; set; }
-  }
-  public class ApplicationCreateResponse
-  {
-    public string Id { get; set; }
-    public string Url { get; set; }
-
-    /// <summary>
-    /// Get the string presentation of the object
-    /// </summary>
-    /// <returns>String presentation of the object</returns>
-    public override string ToString()
-    {
-      var sb = new StringBuilder();
-      sb.Append("ApplicationCreateResponse {\n");
-      sb.Append("  Id: ").Append(Id).Append("\n");
-      sb.Append("  Url: ").Append(Url).Append("\n");
-      sb.Append("}\n");
-      return sb.ToString();
-    }
-  }
   public class Program
   {
     public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
@@ -206,8 +81,14 @@ namespace AwesomeBusinessApp
       var payload = new ApplicationRequest
       {
         Amount = 5000,
-        Organization = new ApplicationOrganization { Email = "admin@a-company.com", NationalOrganizationNumber = "2344473928" },
-        Applicant = new ApplicationApplicant { Email = "joe.smith@a-company.com", Phone = "+4688675309", NationalIdentificationNumber = "2344473928" }
+        Organization = new SimpleOrganization { 
+          Email = "admin@a-company.com", 
+          NationalOrganizationNumber = "2344473928" 
+        },
+        Applicant = new SimpleApplicant { 
+          Email = "joe.smith@a-company.com", 
+          Phone = "+4688675309"
+        }
       };
       Console.WriteLine(payload.ToJson());
       var response = await client.PostAsync("https://api.qred.com/loans/v1/applications", new StringContent(payload.ToJson(), Encoding.UTF8, "application/json"));
