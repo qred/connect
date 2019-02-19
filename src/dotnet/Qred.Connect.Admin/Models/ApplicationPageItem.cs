02b6fdce978c102c80214942f89378b3554a152e
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-02-19 13:56:52 +0100

    Add .net source (#30)

---
+++ src/dotnet/Qred.Connect.Admin/Models/ApplicationPageItem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Qred.Connect.Admin
+{
+  public class ApplicationPageItem
+  {
+    public string Id { get; set; }
+    public string Source { get; set; }
+    public Uri Url { get; set; }
+  }
+}
---
+++ src/dotnet/Qred.Connect.Admin/Models/ApplicationsPage.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace Qred.Connect.Admin
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public partial class ApplicationsPage 
+    { 
+        /// <summary>
+        /// Gets or Sets Total
+        /// </summary>
+        public int Total { get; set; }
+
+        /// <summary>
+        /// Gets or Sets PerPage
+        /// </summary>
+        public int PerPage { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Pages
+        /// </summary>
+        public int Pages { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Page
+        /// </summary>
+        public int Page { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Items
+        /// </summary>
+        public List<ApplicationPageItem> Items { get; set; }
+    }
+    public class ApplicationSource
+    {
+
+    }
+}
